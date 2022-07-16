5621ff4190b59f122986f1c8fee65b2a275eacc9
于方晨 <69724288@qq.com>
2022-07-16 15:27:47 +0800

    xzd新增dockerfile

--- WebApplication1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("611875aa-d81d-40ed-bca1-b8591ab9ac20")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
